8ba793c744d2ff59253fe503fd698a50bd675dd7
HKP102004 <harsh15062004@gmail.com>
2025-12-06 16:19:37 +0530

    feat: add score_text text variable in uimanager

---
+++ Assets/UI_manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_manager : MonoBehaviour
+{
+    [SerializeField]
+    private Text score_txt; //to keep the track of score to display
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
