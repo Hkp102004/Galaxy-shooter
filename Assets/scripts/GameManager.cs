14403fd5fad1ae7daaafb6dc411b4df514816f76
HKP102004 <harsh15062004@gmail.com>
2025-12-07 23:30:21 +0530

    feat: add GameManger component in hierarchy and GameManagement script

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool gameovr = false;
+
+    private void Update()
+    {
+        
+    }
+
+    public void Gameover()
+    {
+        
+    }
+}
