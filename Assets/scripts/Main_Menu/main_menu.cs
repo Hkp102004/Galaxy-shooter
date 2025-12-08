a9b03e694cdcdc94aa766c3f33b28095ea7c6968
HKP102004 <harsh15062004@gmail.com>
2025-12-08 18:34:38 +0530

    feat: add function to load game to the newgame button

--- Assets/scripts/Main_Menu/main_menu.cs
+++ Assets/scripts/Main_Menu/main_menu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class main_menu : MonoBehaviour
 {
-    // Start is called before the first frame updat
+    // Start is called before the first frame update
+
+    public void new_game()
+    {
+        SceneManager.LoadScene(1); //game scene  
+    }
 
 }
