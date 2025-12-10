c8f0735d96c9cf656a2724e9daf7a2507b391ddb
HKP102004 <harsh15062004@gmail.com>
2025-12-10 05:42:59 +0530

    feat: add destroy self in the start function of 2sec in explosion script

--- Assets/scripts/explosion.cs
+++ Assets/scripts/explosion.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class explosion : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(this.gameObject, 2f); //destroy self in 2 sec
     }
 }
