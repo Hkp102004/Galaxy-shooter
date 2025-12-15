91b82ab29bea4ffcf18599590b6eba9a52c8f0ef
HKP102004 <harsh15062004@gmail.com>
2025-12-16 00:54:30 +0530

    feat(player script): play laser_sound whenever laser prefab is instantiated

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -174,10 +174,12 @@ public class Player : MonoBehaviour
         if(trippleactive==true)
         {
             Instantiate(tripplelaser_prefab, transform.position, Quaternion.identity);
+            laser_sound.Play();
         }
         else
         {
             Instantiate(laserprefab, transform.position + new Vector3(0, 0.9f, 0), Quaternion.identity);
+            laser_sound.Play();
         }
     }
 
